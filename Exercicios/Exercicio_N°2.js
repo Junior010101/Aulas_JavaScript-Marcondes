8555d5cc2ac8177c7e2172968e5f73946b65d75d
ALUNO SESI PAULISTA <aluno.sesipaulista@SISTEMAFIEPE.LOCAL>
2024-10-24 14:57:02 -0300

    Adição do Exercico N°2

--- Exercicios/Exercicio_N°2.js
+++ Exercicios/Exercicio_N°2.js
@@ -3,4 +3,23 @@ const prompt = require('prompt-sync')();
 console.clear();
 
 //variaveis
+const time = new Date();
+const dia = time.getDate();
+const mes = time.getMonth();
+const ano = time.getFullYear();
+var DataDeNascimento;
+var DataAtual;
+var idade;
+var nome;
+
+//entrada
+console.log(`${"=".repeat(12)} Algoritimo que converte real para dolar ${"=".repeat(12)}\n`);
+nome = prompt("Digite seu nome: ");
+
+console.log("\nDigite sua data de nascimento com dia, mês e ano separados por '-' deste jeito: 00-00-0000");
+DataDeNascimento = prompt("Data de nascimento: ");
+
+DataDeNascimento = DataDeNascimento.replaceAll("-", "");
+DataAtual = Number(new String(ano+mes+dia));
+
 
